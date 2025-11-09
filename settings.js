b70438c9b6682d970bb7cfba22fda31163f89bcc
riswanbasheer566-max <riswanbasheer566@gmail.com>
2025-11-09 22:45:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'pack of abu 🥵',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Abu Bot",
+  botOwner: 'Riswan', // Your name
+  ownerNumber: '919526862271', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.1",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://chat.whatsapp.com/Ej8cVGNcUbMC5S62YTEdCA?mode=wwt",
 };
 
 module.exports = settings;
